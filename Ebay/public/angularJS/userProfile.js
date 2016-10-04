180b9f1b9119bc0af8251fc2c02aba4f48199dd5
gaurangmhatre <gaurangmhatre@gmail.com>
2016-10-04 00:57:41 -0700

    cart2

--- Ebay/public/angularJS/userProfile.js
+++ Ebay/public/angularJS/userProfile.js
@@ -1,52 +1,52 @@
 var userProfile = angular.module('userProfile', ['ui.router']);
 
-
-userProfile.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-		$locationProvider.html5Mode(true);
-		$stateProvider.state('userProfile', {	
-			url : '/userProfile',
-			views: {
-	            'header': {
-	                templateUrl : 'templates/userProfile/header.html',
-	                
-	            },
-	            'accountDetails': {
-	                templateUrl : 'templates/userProfile/accountDetails.html',
-	                controller: 'accountDetailsController'
-	            },
-	            'activity': {
-	                templateUrl : 'templates/userProfile/activity.html',
-	                controller: 'activityController'
-	            },
-	            'cart': {
-	                templateUrl : 'templates/userProfile/cart.html',
-	                controller: 'cartController'
-	            },
-	            'sellItem': {
-	                templateUrl : 'templates/userProfile/sellItem.html',
-	                controller: 'sellItemController'
-	            },
-			}
-		})
-		$urlRouterProvider.otherwise('/');
-	});
-
-
 /*
-userProfile.controller('accountDetailsController',function($scope, $http,$state){
-	
-	
-	
-});*/
+userProfile.config(
+            function ($routeProvider) {
+                $routeProvider.                 
+                    when('/accountDetails', {
+                        templateUrl: '/public/templates/userProfile/accountDetails.html',
+                        controller: 'accountDetailsController'
+                    }).
+                    when('/activity', {
+                        templateUrl: '/public/templates/userProfile/activity.html',
+                        controller: 'activityController'
+                    }).
+                    when('/cart', {
+                        templateUrl: '/public/templates/userProfile/cart.html',
+                        controller: 'cartController'
+                    }).
+                    when('/sellItem', {
+                        templateUrl: '/public/templates/userProfile/sellItem.html',
+                        controller: 'sellItemController'
+                    });
+            });
 
-
-userProfile.controller('activityController',function($scope, $http,$state){
+*/
+userProfile.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	$stateProvider.state('userProfile', {	
+		url : '/userProfile',
+		views: {
+            'header': {
+                templateUrl : '/templates/userProfile/header.html',
+            },
+            'cart': {
+                templateUrl : '/templates/userProfile/cart.html',
+            },
+		}
+	})
+	$urlRouterProvider.otherwise('/');
 });
 
 
-userProfile.controller('cartController',function($scope, $http,$state){
+
+userProfile.controller('activityController',function($scope, $http, $state){
 });
 
 
-userProfile.controller('sellItemController',function($scope, $http,$state){
+
+
+
+userProfile.controller('sellItemController',function($scope, $http, $state){
 });
